8a45eb27710a9687d838f7d50c0181c8c7fd0575
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-05-29 15:11:28 +0200

    Added code

---
+++ code.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc, matthews_corrcoef
+
+# Utility functions you will re-use
+
+# Data-related utility functions
+def load_blosum(filename):
+    """
+    Read in BLOSUM values into matrix.
+    """
+    aa = ['A', 'R', 'N' ,'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'X']
+    df = pd.read_csv(filename, sep='\s+', comment='#', index_col=0)
+    return df.loc[aa, aa]
+
+def load_peptide_target(filename):
+    """
+    Read amino acid sequence of peptides and
+    corresponding log transformed IC50 binding values from text file.
+    """
+    df = pd.read_csv(filename, sep='\s+', usecols=[0,1], names=['peptide','target'])
+    return df.sort_values(by='target', ascending=False).reset_index(drop=True)
+
+def encode_peptides(Xin):
+    """
+    Encode AA seq of peptides using BLOSUM50.
+    Returns a tensor of encoded peptides of shape (batch_size, MAX_PEP_SEQ_LEN, n_features)
+    """
+    blosum = load_blosum(blosum_file)
+    
+    batch_size = len(Xin)
+    n_features = len(blosum)
+    
+    Xout = np.zeros((batch_size, MAX_PEP_SEQ_LEN, n_features), dtype=np.int8)
+    
+    for peptide_index, row in Xin.iterrows():
+        for aa_index in range(len(row.peptide)):
+            Xout[peptide_index, aa_index] = blosum[ row.peptide[aa_index] ].values
+            
+    return Xout, Xin.target.values
+
+# Misc. functions
+def invoke(early_stopping, loss, model, implement=False):
+    if implement == False:
+        return False
+    else:
+        early_stopping(loss, model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            return True
+        
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+ALLELE = 'A0201' #'A0301'
+
+blosum_file = '../data/BLOSUM50'
+train_data = f'../data/{ALLELE}/train_BA'
+valid_data = f'../data/{ALLELE}/valid_BA'
+test_data = f'../data/{ALLELE}/test_BA'
+
+train_raw = load_peptide_target(train_data)
+valid_raw = load_peptide_target(valid_data)
+test_raw = load_peptide_target(test_data)
